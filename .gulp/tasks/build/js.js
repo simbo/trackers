bda79731e5dc6791a7a3ceda88f6c58182d78ae4
Simon Lepel <simbo@simbo.de>
2015-09-29 03:41:56 +0200

    revise js build task; use browserify instead of gulp-browserify; add es6 transform via babelify

--- .gulp/tasks/build/js.js
+++ .gulp/tasks/build/js.js
@@ -1,36 +1,50 @@
 'use strict';
 
+var path = require('path');
+
+var babelify = require('babelify'),
+    browserify = require('browserify'),
+    eventStream = require('event-stream'),
+    glob = require('glob'),
+    vinylBuffer = require('vinyl-buffer'),
+    vinylSource = require('vinyl-source-stream');
+
 module.exports = [
 
     'build javascripts',
 
-    function() {
-        return this.gulp
-
-            .src(this.paths.js + '/*.js')
-
-            .pipe(this.plugins.browserify({
-                insertGlobals: false,
-                debug: true
-            }))
-
-            .pipe(this.plugins.sourcemaps.init({
-                loadMaps: true
-            }))
-
-            .pipe(this.env === 'production' ?
-                this.plugins.uglify() : this.util.noop()
-            )
-
-            .pipe(this.plugins.sourcemaps.write('.', {
-                includeContent: true,
-                sourceRoot: '.'
-            }))
-
-            .pipe(this.gulp.dest(this.paths.jsDest))
-
-            .on('error', this.log.error);
-
+    function(done) {
+        glob(path.join(this.paths.js, '*.js'), function(err, files) {
+            if (err) {
+                this.log.error(err);
+                return;
+            }
+            eventStream.merge(files.map(function(file) {
+                return browserify({
+                    entries: [file],
+                    insertGlobals: false,
+                    debug: true,
+                    transform: [
+                        babelify
+                    ]
+                })
+                    .bundle().on('error', this.log.error)
+                    .pipe(vinylSource(path.relative(this.paths.js, file)))
+                    .pipe(vinylBuffer())
+                    .pipe(this.plugins.sourcemaps.init({
+                        loadMaps: true
+                    }))
+                    .pipe(this.env === 'production' ?
+                        this.plugins.uglify() : this.util.noop()
+                    )
+                    .pipe(this.plugins.sourcemaps.write('.', {
+                        includeContent: true,
+                        sourceRoot: '.'
+                    }))
+                    .pipe(this.gulp.dest(this.paths.jsDest))
+                    .on('error', this.log.error);
+            }.bind(this))).on('end', done);
+        }.bind(this));
     }
 
 ];
