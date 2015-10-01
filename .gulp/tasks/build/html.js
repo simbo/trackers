52e81caf0b47dbf0de9b1e8c75d8e72024322456
Simon Lepel <simbo@simbo.de>
2015-10-01 10:48:49 +0200

    add jade filter to strip whitespace from template

--- .gulp/tasks/build/html.js
+++ .gulp/tasks/build/html.js
@@ -2,17 +2,30 @@
 
 var path = require('path');
 
+var jade = require('jade');
+
 module.exports = [
 
     'build html',
 
     function() {
+
+        jade.filters.stripWhitespace = function(str) {
+            return this.env !== 'production' ?
+                str : str.split(/[\n\r]/).reduce(function(lines, line) {
+                    line = line.trim();
+                    return lines.concat(line.length > 0 ? [line] : []);
+                }, []).join(' ');
+        }.bind(this);
+
         this.gulp
             .src(path.join(this.paths.src, '**/*.jade'))
             .pipe(this.plugins.jade({
+                jade: jade,
                 pretty: this.env !== 'production'
             }))
             .pipe(this.gulp.dest(this.paths.dest));
+
     }
 
 ];
