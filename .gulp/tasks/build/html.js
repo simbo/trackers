00c61fa34b466224972fe7a0f66406dc4e9fd6c1
Simon Lepel <simbo@simbo.de>
2015-10-05 09:10:01 +0200

    add error handler to jade parser

--- .gulp/tasks/build/html.js
+++ .gulp/tasks/build/html.js
@@ -18,8 +18,8 @@ module.exports = [
         this.gulp
             .src(path.join(this.paths.src, '**/*.jade'))
             .pipe(this.plugins.jade({
-            }))
                 jade: jade
+            }).on('error', this.log.error))
             .pipe(this.gulp.dest(this.paths.dest));
 
     }
